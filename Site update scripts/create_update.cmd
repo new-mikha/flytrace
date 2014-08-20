6fd93358ea5f3acf5eec2b24e2a83d1189e2b430
Michael <mikha@flytrace.com>
2014-08-20 18:45:47 +1000

    Change the script so it can be run by double click
    From now on if it's started without parameters, it
    just asks for the 'from' revision.

--- Site update scripts/create_update.cmd
+++ Site update scripts/create_update.cmd
@@ -1,40 +1,75 @@
 @ECHO OFF
 
-if [%1]==[] goto usage
+echo Create a folder with files updated between two revisions.
 
-SET to_rev=%2
-if [%2]==[] SET to_rev=HEAD
+if [%1]==[-help] goto :usage
+if [%1]==[/help] goto :usage
+if [%1]==[\help] goto :usage
+if [%1]==[-?] goto :usage
+if [%1]==[/?] goto :usage
+if [%1]==[\?] goto :usage
 
-SET dest_folder=%3
-if [%3]==[] SET dest_folder=Update
+SET from_rev=
+SET to_rev=
+SET dest_folder=
 
-SET from_rev=%1
 
-rem echo 1: %from_rev%
-rem echo 2: %to_rev%
-rem echo 3: %dest_folder%
+if [%1]==[] (
+	echo Inclusive 'to' revision is HEAD, dest folder is ./Update
+    SET /P from_rev=Enter NON-inclusive 'from' revision, could be a tag name:
+	
+	SET to_rev=HEAD
+	SET dest_folder=Update
+) else (
+    SET from_rev=%1
 
+    SET to_rev=%2
+    if [%2]==[] SET to_rev=HEAD
+
+    SET dest_folder=%3
+    if [%3]==[] SET dest_folder=Update
+)
+
+if [%from_rev%]==[] goto :usage
+
+echo From: %from_rev%
+echo To: %to_rev%
+echo Folder: %dest_folder%
+
+echo git diff --name-only %from_rev% %to_rev% --diff-filter=ACMR 
 git diff --name-only %from_rev% %to_rev% --diff-filter=ACMR >changed_files.txt
 if ERRORLEVEL 1 (
-	goto :cleanup
+	echo.
+    goto :cleanup
 )
 
 powershell Powershell.exe -executionpolicy remotesigned -File  %~sdp0get-update.ps1 -dest_folder "'%dest_folder%'"
 
 if ERRORLEVEL 1 (
-	goto :cleanup
+	echo.
+    goto :cleanup
 )
 
 goto :cleanup
 
 :usage
-echo Create a folder with files updated between two revisions.
-echo Usage: %0 from_revision [to_revision] [dest_folder]
-echo     If to_revision is omitted, then HEAD is used.
-echo     If dest_folder is omitted, then "Update" is used.
-echo     Note that dest_folder is not cleaned up. If the folder already 
-echo     exists, it's recommended to clean it up manually.
+echo Usage: 
+echo    Either run it without parameters, e.g. right from the Windows Explorer 
+echo    by the double click:
+echo    %0
+echo      If this bach file is running without parameters, it asks for
+echo      'from' revision, and sets 'to' revision and destination 
+echo      folder to defaults (see below)
+echo    - OR run it from the command line: -
+echo    %0 from_revision [to_revision] [dest_folder]
+echo      If to_revision is omitted, then HEAD is used.
+echo      If dest_folder is omitted, then 'Update' is used.
+echo      Note that dest_folder is not cleaned up. If the folder already 
+echo      exists, it's recommended to clean it up manually.
+pause
 exit /B 1
 
 :cleanup
 if EXIST changed_files.txt del changed_files.txt
+
+if [%1]==[] pause
