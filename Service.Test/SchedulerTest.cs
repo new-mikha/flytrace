671b7cece86a3f6399a5904f13c031bc50554a9b
Michael <mikha@flytrace.com>
2014-10-26 12:19:42 +1100

    Iron up tests formatting

--- Service.Test/SchedulerTest.cs
+++ Service.Test/SchedulerTest.cs
@@ -1,13 +1,33 @@
-﻿using System.Collections.Generic;
+﻿/******************************************************************************
+ * Flytrace, online viewer for GPS trackers.
+ * Copyright (C) 2011-2014 Mikhail Karmazin
+ * 
+ * This file is part of Flytrace.
+ * 
+ * Flytrace is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * Flytrace is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Flytrace.  If not, see <http://www.gnu.org/licenses/>.
+ *****************************************************************************/
+
+using System.Collections.Generic;
 using System;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+
 using FlyTrace.LocationLib;
 using FlyTrace.LocationLib.ForeignAccess;
-using FlyTrace.LocationLib.ForeignAccess.Spot;
 using FlyTrace.Service;
-
+using FlyTrace.Service.RequestsSchedule;
 using NUnit.Framework;
 
 namespace Service.Test
