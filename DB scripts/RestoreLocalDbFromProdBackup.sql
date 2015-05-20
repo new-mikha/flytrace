bef43ee694f7f361cc8b295dbf219c24881790cd
Michael <mikha@flytrace.com>
2015-05-20 19:22:39 +1000

    Utility script to restore LocalDB from Prod backup

---
+++ DB scripts/RestoreLocalDbFromProdBackup.sql
@@ -0,0 +1,12 @@
+﻿-- A template to restore LocalDB database in project from the SQL Server backup.
+-- Change pathes & DB part names in 'MOVE' clauses accordingly.
+-- Useful Git commands to apply after succesful the script applied successfully,
+-- just to let Git ignore the changed DB file:
+--      git update-index --assume-unchanged Tracker/App_Data/Flytrace.mdf		
+--      git update-index --assume-unchanged Tracker/App_Data/Flytrace_log.ldf
+
+RESTORE DATABASE [C:\Work\flytrace\Tracker\App_Data\Flytrace.mdf]
+FROM DISK = 'C:\Temp\Flytrace.bak'
+WITH REPLACE,
+MOVE 'Tracker' TO 'C:\Work\flytrace\Tracker\App_Data\Flytrace.mdf',
+MOVE 'Tracker_Log' TO 'C:\Work\flytrace\Tracker\App_Data\Flytrace_log.ldf'
